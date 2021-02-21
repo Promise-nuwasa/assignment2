347dac6c4409b5635e1b6866fef315fe12e54dd3
Promise-nuwasa <promisenuwasasira@gmail.com>
2021-02-21 09:52:21 +0300

    first commit

---
+++ src/HELLO.kt
@@ -0,0 +1,20 @@
+fun main() {
+var studentName=("Nuwasasira Promise")
+    println (studentName)
+    var age=("20")
+    println (age)
+    val phoneNumber=("0782784005")
+    println (phoneNumber)
+    var weight=("50kg")
+    println (weight)
+    var citizen=("false")
+    println (citizen)
+
+
+
+
+
+
+
+
+}
