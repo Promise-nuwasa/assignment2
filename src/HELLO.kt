d48d6c8770ae55fe2c47571cfdabe48249137dd1
Promise-nuwasa <promisenuwasasira@gmail.com>
2021-02-28 12:29:32 +0300

    first commit

--- src/HELLO.kt
+++ src/HELLO.kt
@@ -1,12 +1,34 @@
-fun main() {
-var studentName=("Nuwasasira Promise")
-    println (studentName)
-    var age=("20")
-    println (age)
-    val phoneNumber=("0782784005")
-    println (phoneNumber)
-    var weight=("50kg")
-    println (weight)
-    var citizen=("false")
-    println (citizen)
+import kotlin.system.exitProcess
+
+fun main(){
+   promise()
+    division()
+    add()
+    about()
+
+
+}
+fun promise(){
+    var name="Hello promise"
+     println(name)
+}
+
+
+fun division(){
+var num1 =25
+var num2 =4
+var modulus =num1%num2
+println(modulus)
+}
+fun add(){
+    var num1=10
+    var num2=20
+    var num3=30
+    var num4=40
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+fun about() {
+    var myself =("am smart")
+    println(myself)
 }
